f7b981de2dddee0370287c82bff991090541caed
brandanflores <brandan.f@icloud.com>
2023-08-17 16:31:58 -0600

    fixed code added weather api functionality

--- assets/weather.js
+++ assets/weather.js
@@ -1,6 +1,7 @@
 var myHeaders = new Headers();
 myHeaders.append("X-RapidAPI-Host", "weatherapi-com.p.rapidapi.com");
 myHeaders.append("X-RapidAPI-Key", "359cad1366msh63f358d57b233a3p1edbd6jsn942828c15f06");
+var weatherSection =  document.getElementById('weatherSection')
 
 var requestOptions = {
   method: 'GET',
@@ -10,5 +11,15 @@ var requestOptions = {
 
 fetch("https://weatherapi-com.p.rapidapi.com/forecast.json?q=Denver&days=3", requestOptions)
   .then(response => response.json())
-  .then(result => console.log(result))
+  .then(result => {
+    console.log(result.current);
+    var data = result.current
+
+    var lastUpdated = document.createElement('p')
+
+    lastUpdated.textContent = 'last updated: ' + data.last_updated
+    lastUpdated.classList.add('date_weather')
+
+    weatherSection.append(lastUpdated)
+  })
   .catch(error => console.log('error', error));
